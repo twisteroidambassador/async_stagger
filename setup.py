5cdbded7ead35e5aaae09fd17b2d03806fc84d35
twisteroid ambassador <twisteroidambassador@users.noreply.github.com>
2018-06-18 11:33:50 +0800

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'rt') as readme_file:
 
 setup(
     name='async_stagger',
-    version='0.1.1',
+    version='0.1.2',
     description='Happy eyeballs and underlying scheduling algorithm in asyncio',
     long_description=long_description,
     long_description_content_type='text/x-rst',
