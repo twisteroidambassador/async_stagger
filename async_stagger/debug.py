0690bdd884ba2e91100bcbcbfe730710dceee94f
twisteroid ambassador <twisteroidambassador@users.noreply.github.com>
2018-10-24 19:44:53 +0800

    Better debug logging in Python 3.8.

--- async_stagger/debug.py
+++ async_stagger/debug.py
@@ -1,4 +1,9 @@
+import asyncio
 import logging
+import sys
+
+
+PY38 = sys.version_info >= (3, 8)
 
 
 AS_DEBUG = False
@@ -14,6 +19,13 @@ def set_debug(enabled):
     AS_DEBUG = enabled
 
 
-def debug_log(*args, **kwargs):
-    if AS_DEBUG:
-        logger.debug(*args, **kwargs)
+def debug_log(msg, *args, **kwargs):
+    if not AS_DEBUG:
+        return
+
+    if PY38:
+        # In Python 3.8, Tasks have unique names. Printing these names may help
+        # debugging, but it also makes the logs much more verbose.
+        logger.debug('%r ' + msg, asyncio.current_task(), *args, **kwargs)
+    else:
+        logger.debug(msg, *args, **kwargs)
