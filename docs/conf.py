809fed753c94d748ea9ed74b2a842df0e90ac1c4
twisteroid ambassador <twisteroidambassador@users.noreply.github.com>
2024-04-03 10:43:32 +0800

    Update readthedocs configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'async_stagger'
-copyright = '2018, twisteroid ambassador'
+copyright = '2024, twisteroid ambassador'
 author = 'twisteroid ambassador'
 
 # The short X.Y version
@@ -62,7 +62,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
