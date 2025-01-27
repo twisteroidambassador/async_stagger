084461810c2182f6c59b444226f4a456755c3b4a
twisteroid ambassador <twisteroidambassador@users.noreply.github.com>
2025-01-27 17:16:42 +0800

    Run tests with all available event loops and with/without eager task factory.
    This will find all available native event loops (2 on Windows) and uvloop if installed.
    
    Revisit this when pytest-asyncio has come up with a new solution for the deprecation of *EventLoopPolicy.

---
+++ async_stagger/conftest.py
@@ -0,0 +1,49 @@
+import asyncio
+
+import pytest
+
+
+# Find all available event loop policies in asyncio, and in uvloop if installed
+available_policies = [
+    getattr(asyncio, p)
+    for p in dir(asyncio)
+    if p.endswith('EventLoopPolicy')
+    and not p.startswith('Abstract')
+    and not p.startswith('Default')
+]
+
+try:
+    import uvloop
+
+    # Give uvloop's policy a more recognizable name
+    class UvloopEventLoopPolicy(uvloop.EventLoopPolicy):
+        pass
+
+    available_policies.append(UvloopEventLoopPolicy)
+except ImportError:
+    pass
+
+
+# Create a new version of each available policy that uses eager task factory
+def create_eager_policy(policy: asyncio.AbstractEventLoopPolicy) -> asyncio.AbstractEventLoopPolicy:
+    class NewEagerPolicy(policy):
+        def new_event_loop(self):
+            loop = super().new_event_loop()
+            loop.set_task_factory(asyncio.eager_task_factory)
+            return loop
+    
+    NewEagerPolicy.__name__ = f'Eager{policy.__name__}'
+    
+    return NewEagerPolicy
+
+
+eager_policies = []
+for policy in available_policies:
+    eager_policies.append(create_eager_policy(policy))
+available_policies.extend(eager_policies)
+
+
+# Run each test with each policy
+@pytest.fixture(params=available_policies)
+def event_loop_policy(request):
+    return request.param()
